85da71efbf5a8147fead92f9a41033122ff756c2
versarje <87492954+versarje@users.noreply.github.com>
2026-09-26 21:25:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def webhook():
 
 if __name__ == "__main__":
     init_d1_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
